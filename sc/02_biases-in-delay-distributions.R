4fb684c089423373619d2ad40810664599e00acc
Louie Gold <tlouiegold@gmail.com>
2025-07-16 15:31:33 -0400

    Created infection time delay df

--- sc/02_biases-in-delay-distributions.R
+++ sc/02_biases-in-delay-distributions.R
@@ -5,3 +5,13 @@ library("tidyr")
 library("purrr")
 library("lubridate")
 library("tidybayes")
+
+set.seed(123)
+options(cmdstanr_print_line_numbers = TRUE)
+
+df <- add_delays(
+  infection_times,
+  hosp_params = list(meanlog = 1.0, sdlog = 0.5)
+)
+
+head(df)
